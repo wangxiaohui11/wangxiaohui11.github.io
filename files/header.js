381a4afb30fb171ea99e98cf3593bb10128a2252
home <wangxiaohui11@hotmail.com>
2022-06-01 15:41:16 +0800

    update

---
+++ files/header.js
@@ -0,0 +1,30 @@
+document.getElementById("header").innerHTML = '\
+		<div class="header" style="padding-top:20px; padding-bottom:5px; background:#d2d2d2">\
+			<!-- <img id="header-img" src="" width="800px"> -->\
+			<center>\
+				<img id="profile-img" src="./files/img/profile_xiaohui.jpg"></img>\
+				<h1><strong>XIAOHUI WANG</strong></h1>\
+				<!-- <p><a href="mailto:wangxiaohui11@hotmail.com"><span style="color:#336699">wangxiaohui11@hotmail.com</span></a></p> -->\
+			</center>\
+		</div>\
+		\
+		<div class="header" style="padding-top:10px; padding-bottom:10px; background:#e2e2e2">\
+			<center>\
+					<a href="mailto:wangxiaohui11@hotmail.com"><img src="./files/img/logos/email.png" height="30"/></a> &nbsp;\
+					<a href="https://scholar.google.com/citations?view_op=list_works&hl=en&hl=en&user=BBXxXnkAAAAJ" target="_blank"><img src="./files/img/logos/google_scholar.png" height="30"/></a> &nbsp;\
+					<!--<a href="https://dblp.org/pid/210/5795.html" target="_blank"><img src="./files/img/logos/dblp.png" height="30"/></a> &nbsp;\
+					<a href="https://www.researchgate.net/profile/Xiaohui-Wang-19" target="_blank"><img src="./files/img/logos/researchgate.png" height="30"/></a> &nbsp; -->\
+					<!-- <a href="https://github.com/qiusihang" target="_blank"><img src="./files/img/logos/github.png" height="30"/></a> &nbsp; -->\
+					<!-- <a href="https://twitter.com/qiusihang" target="_blank"><img src="./files/img/logos/twitter.png" height="30"/></a>\
+			</center>\
+		</div>\
+		\
+		<div class="header" style="padding-top:10px; padding-bottom:10px; background:#f2f2f2">\
+			<center>\
+				<table><tr>\
+					<th width="100px"><a style="text-decoration:none" href="index.html">ABOUT</a></th>\
+					<th width="150px"><a style="text-decoration:none" href="publications.html">PUBLICATIONS</a></th>\
+				</tr></table>\
+			</center>\
+		</div>\
+';
